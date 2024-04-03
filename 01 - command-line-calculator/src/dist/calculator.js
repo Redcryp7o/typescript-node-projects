f106aca7bdc4dc34a14603769b69ffaa0ab7938c
RedCryp7o <technicalsumra786@gmail.com>
2024-04-03 12:43:25 +0500

    solved

---
+++ 01 - command-line-calculator/src/dist/calculator.js
@@ -0,0 +1,55 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
+function calculator() {
+    inquirer_1.default.prompt([
+        {
+            type: 'input',
+            name: 'num1',
+            message: 'Enter the first number:'
+        },
+        {
+            type: 'input',
+            name: 'num2',
+            message: 'Enter the second number:'
+        },
+        {
+            type: 'list',
+            name: 'operation',
+            message: 'Choose an operation:',
+            choices: ['Addition', 'Subtraction', 'Multiplication', 'Division']
+        }
+    ]).then(answers => {
+        const num1 = parseFloat(answers.num1);
+        const num2 = parseFloat(answers.num2);
+        let result;
+        switch (answers.operation) {
+            case 'Addition':
+                result = num1 + num2;
+                break;
+            case 'Subtraction':
+                result = num1 - num2;
+                break;
+            case 'Multiplication':
+                result = num1 * num2;
+                break;
+            case 'Division':
+                if (num2 === 0) {
+                    console.log('Error: Division by zero');
+                    return;
+                }
+                result = num1 / num2;
+                break;
+            default:
+                console.log('Invalid operation');
+                return;
+        }
+        console.log('Result:', result);
+    }).catch(error => {
+        console.log('Error:', error);
+    });
+}
+calculator();
